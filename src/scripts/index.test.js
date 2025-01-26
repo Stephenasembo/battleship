bfe033958e0ba6847e86946d7bb9a724e6b8e4b2
Stephen Asembo <165886904+Stephenasembo@users.noreply.github.com>
2025-01-26 16:36:48 +0300

    Test: Check If Ship Is Sunk
    The ship sinks when the number of hits are equal or more than its
    length

--- src/scripts/index.test.js
+++ src/scripts/index.test.js
@@ -7,14 +7,26 @@ class Ship {
     this.#hits += 1;
     return this.#hits;
   }
+  isSunk() {
+    if (this.#hits >= this.length) {
+      return true;
+    }
+    return false;
+  }
 }
 
 describe('Ship objects instantiated correctly', () => {
   let myShip;
   beforeEach(() => {
-    myShip = new Ship;
+    myShip = new Ship(4);
   })
+
   test('hit method works', () => {
     expect(myShip.hit()).toBe(1);
+  })
+
+
+  test('check if ship is sunk', () => {  
+    expect(myShip.isSunk()).toBeTruthy();
   })
 })
