6424595d2a7250ba8f09c15e00c451f64074d679
Stephen Asembo <165886904+Stephenasembo@users.noreply.github.com>
2025-01-26 16:38:30 +0300

    Feat: Use Ship Class For Ship Object Instantiation

---
+++ src/scripts/index.js
@@ -0,0 +1,16 @@
+class Ship {
+  #hits = 0;
+  constructor(length) {
+    this.length = length;
+  }
+  hit() {
+    this.#hits += 1;
+    return this.#hits;
+  }
+  isSunk() {
+    if (this.#hits >= this.length) {
+      return true;
+    }
+    return false;
+  }
+}
