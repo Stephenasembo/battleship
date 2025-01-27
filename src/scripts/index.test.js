300ea5ab09489a93f2f5428bc530b3dfc12494d4
Stephen Asembo <165886904+Stephenasembo@users.noreply.github.com>
2025-01-27 17:49:04 +0300

    Test: Area Occupied By Ship Should Match It's Size
    A ship of size 4 should occupy 4 spots on the board

--- src/scripts/index.test.js
+++ src/scripts/index.test.js
@@ -63,4 +63,8 @@ describe('Gameboard places ships on game board', () => {
   test('Board creates players ships', () => {
     expect(board.createPlayerShip().length).toBe(10);
   })
+  test('Board places ship of size 4 correctly', () => {
+    const markedLocation = [[0, 0], [0, 1], [0, 2], [0, 3]]
+    expect(board.placeShip().length).toBe(markedLocation.length);
+  })
 });
