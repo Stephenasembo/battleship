934570712c606585aee68b5bdd964253ea267713
Stephen Asembo <165886904+Stephenasembo@users.noreply.github.com>
2025-01-27 16:32:41 +0300

    Feat: Create Gameboard
    The gameboard has 10*10 spots

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -14,3 +14,18 @@ class Ship {
     return false;
   }
 }
+
+function createBoard() {
+  const board = [];
+  for (let i = 0; i < 10; i += 1) {
+    const row = [];
+    for (let j = 0; j < 10; j += 1) {
+      const col = [];
+      row.push(col);
+    }
+    board.push(row);
+  }
+  return board;
+}
+
+export default createBoard;
