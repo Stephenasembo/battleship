a94c5cbf2e57d6571e6b01b929a9ec8b3fc0af99
Stephen Asembo <165886904+Stephenasembo@users.noreply.github.com>
2025-01-27 16:25:54 +0300

    Test: Board Has 10*10 Spots

--- src/scripts/index.test.js
+++ src/scripts/index.test.js
@@ -1,3 +1,5 @@
+import createBoard from './index';
+
 class Ship {
   #hits = 0;
   constructor(length) {
@@ -27,6 +29,19 @@ describe('Ship objects instantiated correctly', () => {
 
 
   test('check if ship is sunk', () => {  
-    expect(myShip.isSunk()).toBeTruthy();
+    expect(myShip.isSunk()).toBeFalsy();
   })
 })
+
+describe('Gameboard works correctly', () => {
+  let board;
+  beforeEach(() => {
+    board = createBoard();
+  })
+  test('board has 10 rows', () => {
+    expect(board.length).toBe(10);
+  })
+  test('each row has 10 columns', () => {
+    expect(board[0].length).toBe(10);
+  })
+});
