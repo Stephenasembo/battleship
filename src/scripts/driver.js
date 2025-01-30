a99916a173026820e8e794f037f2287024914b76
Stephen Asembo <165886904+Stephenasembo@users.noreply.github.com>
2025-01-30 16:52:30 +0300

    Feat: Display Ship Position On The Board

--- src/scripts/driver.js
+++ src/scripts/driver.js
@@ -6,24 +6,49 @@ const player2 = Player('computer');
 
 function displayBoard(parent) {
   const boardDiv = document.createElement('div');
+  let boardName;
+  if (parent.classList[1] === 'one') {
+    boardName = 'p1';
+  } else if (parent.classList[1] === 'two') {
+    boardName = 'p2';
+  }
   for (let i = 0; i < 10; i += 1) {
     const rowDiv = document.createElement('div');
-    rowDiv.setAttribute('id', i);
+    rowDiv.setAttribute('id', `${boardName}row${i}`);
     for (let j = 0; j < 10; j += 1) {
       const colBtn = document.createElement('button');
-      colBtn.setAttribute('id', j);
+      colBtn.setAttribute('id', `${boardName}col${j}`);
       rowDiv.appendChild(colBtn);
     }
     boardDiv.appendChild(rowDiv);
   }
   parent.appendChild(boardDiv);
+  return { boardDiv, boardName };
 }
+
+function displayShips(ship, board, playerBoard) {
+  const location = ship.boardLocation;
+  for (let i = 0; i < location.length; i += 1) {
+    let startRow = location[i][0];
+    let startCol = location[i][1];
+    startCol = startCol.toString();
+    startRow = startRow.toString();
+    const displayStartRow = board.querySelector(`#${playerBoard}row${startRow}`);
+    const displayStartCol = displayStartRow.querySelector(`#${playerBoard}col${startCol}`);
+    displayStartCol.style.backgroundColor = 'red';  
+  }
+}
+
+const testShip = new Ship(4);
+testShip.boardLocation = [[0, 0], [0, 1], [0, 2], [0, 3]];
 
 function displayController() {
   const player1Board = document.querySelector('.one');
   const player2Board = document.querySelector('.two');
-  displayBoard(player1Board);
-  displayBoard(player2Board);
+  const board1 = displayBoard(player1Board);
+  const board2 = displayBoard(player2Board);
+  displayShips(testShip, board1.boardDiv, board1.boardName);
+  displayShips(testShip, board2.boardDiv, board2.boardName);
 }
 
 displayController();
