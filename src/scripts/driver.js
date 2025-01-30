d10ca5947ee21249a881a788551f76eaf51cf514
Stephen Asembo <165886904+Stephenasembo@users.noreply.github.com>
2025-01-30 09:26:01 +0300

    Feat!: Add A Driver Script

---
+++ src/scripts/driver.js
@@ -0,0 +1,28 @@
+import { Ship, createBoard, Player } from './index';
+
+const player1 = Player('human');
+const player2 = Player('computer');
+
+function displayBoard(parent) {
+  const boardDiv = document.createElement('div');
+  for (let i = 0; i < 10; i += 1) {
+    const rowDiv = document.createElement('div');
+    rowDiv.setAttribute('id', i);
+    for (let j = 0; j < 10; j += 1) {
+      const colBtn = document.createElement('button');
+      colBtn.setAttribute('id', j);
+      rowDiv.appendChild(colBtn);
+    }
+    boardDiv.appendChild(rowDiv);
+  }
+  parent.appendChild(boardDiv);
+}
+
+function displayController() {
+  const player1Board = document.querySelector('.one');
+  const player2Board = document.querySelector('.two');
+  displayBoard(player1Board);
+  displayBoard(player2Board);
+}
+
+displayController();
